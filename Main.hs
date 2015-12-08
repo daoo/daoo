e0ae81b1eae1b8e249cb41729acbe0acac2d4c65
Daniel Oom <daoo314@gmail.com>
2015-12-08 19:57:17 +0100

    Update map links

--- Main.hs
+++ Main.hs
@@ -141,11 +141,11 @@ compactSite = H.docTypeHtml $ do
 
       H.h1 "Geografiska Kartor"
       H.p $ commasep
-        [ "Lantmäteriet" `ahref` "http://kso2.lantmateriet.se"
-        , "Hitta.se"     `ahref` "http://hitta.se/kartan"
-        , "Eniro"        `ahref` "http://kartor.eniro.se"
-        , "Bing"         `ahref` "https://www.bing.com/maps"
-        , "Google"       `ahref` "https://google.com/maps"
+        [ "lantmateriet.se" `ahref` "http://kso2.lantmateriet.se"
+        , "hitta.se"        `ahref` "http://hitta.se/kartan"
+        , "eniro.se"        `ahref` "http://kartor.eniro.se"
+        , "bing.com"        `ahref` "https://www.bing.com/maps"
+        , "google.com"      `ahref` "https://google.com/maps"
         ]
   where
     commasep = mconcat . intersperse ", "
