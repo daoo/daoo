f5fb457c7cf17f6a63a161761c3236a3c96e0aa0
Daniel Oom <daoo314@gmail.com>
2015-12-08 19:55:23 +0100

    Update some links and add some css

--- Main.hs
+++ Main.hs
@@ -1,35 +1,27 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-import Data.List hiding (group)
+import Data.List
 import Data.Monoid
 import Network.URI.Encode
 import Text.Blaze.Html.Renderer.Utf8
+import qualified Clay
 import qualified Data.ByteString.Char8 as B
 import qualified Data.Map as M
 import qualified Text.Blaze.Html5 as H
 import qualified Text.Blaze.Html5.Attributes as A
 
 data Link = Link
-  { title :: String
-  , url :: String
-  , extra :: String
-  , group :: String
+  { linkTitle :: String
+  , linkUrl :: String
+  , linkExtra :: String
+  , linkGroup :: String
   }
 
-vaderprognosen :: String -> [String -> Link]
-vaderprognosen ident =
-  [ Link "Höjdvind" height ""
-  , Link "Temperaturkurva" temp ""
-  ]
+groupLinks :: [Link] -> M.Map String [Link]
+groupLinks = order . M.fromListWith (++) . assoc
   where
-    height = "http://www.vaderprognosen.se/vader/prognos/hpfcst.php?place=" <> ident <> "&type=0"
-    temp   = "http://www.vaderprognosen.se/vader/prognos/hpfcst.php?place=" <> ident <> "&type=1"
-
-ballong :: Int -> [String -> Link]
-ballong ident = [Link "Ballongväder" ("http://www.ballong.org/drupal/vader/" <> show ident) ""]
-
-windguru :: Int -> [String -> Link]
-windguru ident = [Link "Översiktsprognos" ("http://www.windguru.cz/int/index.php?sc=" <> show ident) ""]
+    assoc = map (\link -> (linkGroup link, [link]))
+    order = M.map (sortOn linkTitle)
 
 pointPrognosis :: [Link]
 pointPrognosis =
@@ -48,110 +40,145 @@ pointPrognosis =
   map ($"Vara") (vaderprognosen "Vara") ++
   map ($"Uddevalla") (vaderprognosen "Uddevalla" ++ windguru 333774)
 
+  where
+    vaderprognosen ident =
+      [ Link "Höjdvind" height ""
+      , Link "Temperaturkurva" temp ""
+      ]
+      where
+        height = "http://www.vaderprognosen.se/vader/prognos/hpfcst.php?place=" <> ident <> "&type=0"
+        temp   = "http://www.vaderprognosen.se/vader/prognos/hpfcst.php?place=" <> ident <> "&type=1"
+
+    ballong, windguru :: Int -> [String -> Link]
+    ballong ident = [Link "Ballongväder" ("http://www.ballong.org/drupal/vader/" <> show ident) ""]
+    windguru ident = [Link "Översiktsprognos" ("http://www.windguru.cz/int/index.php?sc=" <> show ident) ""]
+
 fieldPrognosis :: [Link]
 fieldPrognosis =
-  [ Link { url = "http://www.klart.se/lufttryck",                     title = "Europa",       group = "Lufttryck", extra = "" }
-  , Link { url = "http://www.yr.no/kart/#laga=trykk",                 title = "Skandinavien", group = "Lufttryck", extra = "" }
-  , Link { url = "http://www.vaderradar.se/gfsforecast/LTEuropeWind", title = "Europa",       group = "Lufttryck", extra = "" }
+  [ Link { linkUrl = "http://www.yr.no/kart/#laga=trykk",                 linkTitle = "yr.no",         linkGroup = "Lufttryck", linkExtra = "" }
 
-  , Link { url = "http://se.baltrad.eu",                                            title = "Östersjön",    group = "Radar", extra = "5 min" }
-  , Link { url = "http://www.radareu.cz",                                           title = "Europa",       group = "Radar", extra = "15 min" }
-  , Link { url = "http://www.vaderradar.se/radarscandinavie",                       title = "Norra Europa", group = "Radar", extra = "15 min" }
-  , Link { url = "http://www.blitzortung.org/Webpages/index.php?lang=en&page_0=11", title = "Europa",       group = "Blixt", extra = "Live" }
+  , Link { linkUrl = "http://se.baltrad.eu",                                            linkTitle = "baltrad.eu",      linkGroup = "Radar", linkExtra = "5 min" }
+  , Link { linkUrl = "http://www.radareu.cz",                                           linkTitle = "radareu.cz",      linkGroup = "Radar", linkExtra = "15 min" }
+  , Link { linkUrl = "http://www.blitzortung.org/Webpages/index.php?lang=en&page_0=11", linkTitle = "blitzortung.org", linkGroup = "Blixt", linkExtra = "Live" }
 
-  , Link { url = "http://www.smhi.se/vadret/nederbord-molnighet/radar-blixt-sverige", title = "Sverige", group = "Radar", extra = "15 min (med blixt)" }
-  , Link { url = "http://www.smhi.se/vadret/nederbord-molnighet/satellit-norden-rgb", title = "Norden",  group = "Satellit", extra = "" }
+  , Link { linkUrl = "http://www.smhi.se/vadret/nederbord-molnighet/radar-blixt-sverige", linkTitle = "smhi.se", linkGroup = "Radar",    linkExtra = "15 min (med blixt)" }
+  , Link { linkUrl = "http://www.smhi.se/vadret/nederbord-molnighet/satellit-norden-rgb", linkTitle = "smhi.se", linkGroup = "Satellit", linkExtra = "" }
 
-  , Link { url = "http://www.vaderprognosen.se/vader/prognos/index.php?d=2&a=2&aoi=th1&p=wind10m", title = "Södra Sverige", group = "Vind", extra = "" }
-  , Link { url = "http://www.vaderprognosen.se/vader/prognos/index.php?d=2&a=2&aoi=th1&p=prec",    title = "Södra Sverige", group = "Nederbörd", extra = "" }
-  , Link { url = "http://www.vaderprognosen.se/vader/prognos/index.php?d=2&a=2&aoi=th1&p=cldbase", title = "Södra Sverige", group = "Moln", extra = "" }
+  , Link { linkUrl = "http://www.vaderprognosen.se/vader/prognos/index.php?d=2&a=2&aoi=th1&p=wind10m", linkTitle = "vaderprognosen.se", linkGroup = "Vind",      linkExtra = "" }
+  , Link { linkUrl = "http://www.vaderprognosen.se/vader/prognos/index.php?d=2&a=2&aoi=th1&p=prec",    linkTitle = "vaderprognosen.se", linkGroup = "Nederbörd", linkExtra = "" }
+  , Link { linkUrl = "http://www.vaderprognosen.se/vader/prognos/index.php?d=2&a=2&aoi=th1&p=cldbase", linkTitle = "vaderprognosen.se", linkGroup = "Moln",      linkExtra = "" }
 
-  , Link { url = "http://www.klart.se/europa/nederbörd", title = "Europa",  group = "Nederbörd", extra = "" }
-  , Link { url = "http://www.klart.se/europa/satellit",  title = "Europa",  group = "Satellit", extra = "" }
-  , Link { url = "http://www.klart.se/moln",             title = "Sverige", group = "Moln", extra = "" }
-  , Link { url = "http://www.klart.se/nederb%C3%B6rd",   title = "Sverige", group = "Nederbörd", extra = "" }
-  , Link { url = "http://www.klart.se/nederbörd",        title = "Sverige", group = "Radar", extra = "60 min (med historik)" }
-  , Link { url = "http://www.klart.se/vind",             title = "Sverige", group = "Vind", extra = "" }
+  , Link { linkUrl = "http://www.klart.se/europa/nederbörd", linkTitle = "klart.se (Europa)",  linkGroup = "Nederbörd", linkExtra = "" }
+  , Link { linkUrl = "http://www.klart.se/europa/satellit",  linkTitle = "klart.se",           linkGroup = "Satellit",  linkExtra = "" }
+  , Link { linkUrl = "http://www.klart.se/moln",             linkTitle = "klart.se",           linkGroup = "Moln",      linkExtra = "" }
+  , Link { linkUrl = "http://www.klart.se/nederbörd",        linkTitle = "klart.se (Sverige)", linkGroup = "Nederbörd", linkExtra = "" }
+  , Link { linkUrl = "http://www.klart.se/nederbörd",        linkTitle = "klart.se",           linkGroup = "Radar",     linkExtra = "60 min (med historik)" }
+  , Link { linkUrl = "http://www.klart.se/vind",             linkTitle = "klart.se",           linkGroup = "Vind",      linkExtra = "" }
+  , Link { linkUrl = "http://www.klart.se/lufttryck",        linkTitle = "klart.se",           linkGroup = "Lufttryck", linkExtra = "" }
 
-  , Link { url = "http://www.vaderradar.se/gfsforecast/LTEuropeClouds", title = "Europa",  group = "Moln", extra = "" }
-  , Link { url = "http://www.vaderradar.se/gfsforecast/LTEuropeRain",   title = "Europa",  group = "Nederbörd", extra = "" }
-  , Link { url = "http://www.vaderradar.se/gfsforecast/LTEuropeWind",   title = "Europa",  group = "Vind", extra = "" }
-  , Link { url = "http://www.vaderradar.se/radarscanprognos",           title = "Sverige", group = "Nederbörd", extra = "" }
+  , Link { linkUrl = "http://www.vaderradar.se/gfsforecast/LTEuropeClouds", linkTitle = "vaderradar.se", linkGroup = "Moln",      linkExtra = "" }
+  , Link { linkUrl = "http://www.vaderradar.se/gfsforecast/LTEuropeRain",   linkTitle = "vaderradar.se", linkGroup = "Nederbörd", linkExtra = "" }
+  , Link { linkUrl = "http://www.vaderradar.se/gfsforecast/LTEuropeWind",   linkTitle = "vaderradar.se", linkGroup = "Vind",      linkExtra = "" }
+  , Link { linkUrl = "http://www.vaderradar.se/gfsforecast/LTEuropeWind",   linkTitle = "vaderradar.se", linkGroup = "Lufttryck", linkExtra = "" }
+  , Link { linkUrl = "http://www.vaderradar.se/radarscandinavie",           linkTitle = "vaderradar.se", linkGroup = "Radar",     linkExtra = "15 min" }
+  , Link { linkUrl = "http://www.vaderradar.se/radarscanprognos",           linkTitle = "vaderradar.se", linkGroup = "Nederbörd", linkExtra = "" }
 
-  , Link { url = "http://www.dmi.dk/vejr/til-lands/vejrkort", title = "Danmark", group = "Luftfuktighet", extra = "" }
-  , Link { url = "http://www.dmi.dk/vejr/til-lands/vejrkort", title = "Danmark", group = "Vind", extra = "" }
-  , Link { url = "http://www.dmi.dk/vejr/til-lands/vejrkort", title = "Europa",  group = "Frontkarta", extra = "" }
-  , Link { url = "http://www.dmi.dk/vejr/til-lands/vejrkort", title = "Danmark", group = "Lufttryck", extra = "" }
-
-  , Link { url = "http://www.dmi.dk/vejr/til-lands/landsudsigten",                                           title = "Danmark", group = "Väderöversikt", extra = "" }
-  , Link { url = "http://www.dmi.dk/vejr/til-lands/maaned-og-saeson",                                        title = "Danmark", group = "Månad och Säsong", extra = "" }
-  , Link { url = "http://www.smhi.se/vadret/vadret-i-sverige/vaderoversikt-sverige-meteorologens-kommentar", title = "Sverige", group = "Väderöversikt", extra = "" }
+  , Link { linkUrl = "http://www.dmi.dk/vejr/til-lands/vejrkort", linkTitle = "dmi.dk", linkGroup = "Luftfuktighet", linkExtra = "" }
+  , Link { linkUrl = "http://www.dmi.dk/vejr/til-lands/vejrkort", linkTitle = "dmi.dk", linkGroup = "Vind",          linkExtra = "" }
+  , Link { linkUrl = "http://www.dmi.dk/vejr/til-lands/vejrkort", linkTitle = "dmi.dk", linkGroup = "Frontkarta",    linkExtra = "" }
+  , Link { linkUrl = "http://www.dmi.dk/vejr/til-lands/vejrkort", linkTitle = "dmi.dk", linkGroup = "Lufttryck",     linkExtra = "" }
   ]
 
-main :: IO ()
-main = renderHtmlToByteStringIO B.putStrLn $ H.docTypeHtml $ do
+compactSite :: H.Html
+compactSite = H.docTypeHtml $ do
   H.head $ do
     H.meta H.! A.charset "utf-8"
     H.meta H.! A.content "width=device-width,initial-scale=1" H.! A.name "viewport"
+    H.style (H.toHtml (Clay.renderWith Clay.compact [] style))
     H.title "Flygplanering"
   H.body $ do
-    H.h1 "Punktprognoser"
-    H.table $ mapM_ (uncurry tableRow) (M.assocs (groupLinks pointPrognosis))
+    H.header $ H.h1 "FPC"
 
-    H.h1 "Fältprognoser"
-    H.table $ mapM_ (uncurry tableRow) (M.assocs (groupLinks fieldPrognosis))
+    H.section $ do
+      H.h1 "Punktprognoser"
+      H.table $ mapM_ (uncurry tableRow) (M.assocs (groupLinks pointPrognosis))
 
-    H.h1 "Flygvädret"
-    H.p $ commasep
-      [ H.a "NSWC"    H.! A.href "https://aro.lfv.se/Links/Link/ViewLink?type=MET&TorLinkId=229"
-      , H.a "METAR"   H.! A.href "https://aro.lfv.se/Links/Link/ViewLink?type=MET&TorLinkId=300"
-      , H.a "TAF"     H.! A.href "https://aro.lfv.se/Links/Link/ViewLink?type=MET&TorLinkId=304"
-      ]
+      H.h1 "Fältprognoser"
+      H.table $ mapM_ (uncurry tableRow) (M.assocs (groupLinks fieldPrognosis))
 
-    H.p $ commasep
-      [ H.a "LHP Område A" H.! A.href "https://aro.lfv.se/Links/Link/ViewLink?type=MET&TorLinkId=307"
-      , H.a "LHP Område B" H.! A.href "https://aro.lfv.se/Links/Link/ViewLink?type=MET&TorLinkId=308"
-      , H.a "LHP Område C" H.! A.href "https://aro.lfv.se/Links/Link/ViewLink?type=MET&TorLinkId=309"
-      , H.a "LHP Område D" H.! A.href "https://aro.lfv.se/Links/Link/ViewLink?type=MET&TorLinkId=310"
-      ]
+      H.h1 "Metrologens Kommentarer"
+      H.p $ commasep
+        [ "DMI Vecka"            `ahref` "http://www.dmi.dk/vejr/til-lands/landsudsigten"
+        , "DMI Månad och Säsong" `ahref` "http://www.dmi.dk/vejr/til-lands/maaned-og-saeson"
+        , "SMHI Vecka"           `ahref` "http://www.smhi.se/vadret/vadret-i-sverige/vaderoversikt-sverige-meteorologens-kommentar"
+        ]
 
-    H.p $ commasep
-      [ H.a "AIP SUP" H.! A.href "https://aro.lfv.se/Editorial/View/IAIP?folderId=22"
-      , H.a "NOTAM"   H.! A.href "https://aro.lfv.se/Links/Link/ViewLink?TorLinkId=162&type=AIS"
-      ]
+      H.h1 "Flygvädret"
+      H.p $ commasep
+        [ "NSWC"  `ahref` "https://aro.lfv.se/Links/Link/ViewLink?type=MET&TorLinkId=229"
+        , "METAR" `ahref` "https://aro.lfv.se/Links/Link/ViewLink?type=MET&TorLinkId=300"
+        , "TAF"   `ahref` "https://aro.lfv.se/Links/Link/ViewLink?type=MET&TorLinkId=304"
+        ]
 
-    H.h1 "Väderlänkar"
-    H.p $ commasep
-      [ H.a "Interaktiv väderkarta" H.! A.href "http://earth.nullschool.net"
-      , H.a "Interaktiv väderkarta" H.! A.href"https://www.windyty.com"
-      , H.a "Nördväder" H.! A.href "http://www.wetterzentrale.de"
-      ]
+      H.p $ commasep
+        [ "LHP Område A" `ahref` "https://aro.lfv.se/Links/Link/ViewLink?type=MET&TorLinkId=307"
+        , "LHP Område B" `ahref` "https://aro.lfv.se/Links/Link/ViewLink?type=MET&TorLinkId=308"
+        , "LHP Område C" `ahref` "https://aro.lfv.se/Links/Link/ViewLink?type=MET&TorLinkId=309"
+        , "LHP Område D" `ahref` "https://aro.lfv.se/Links/Link/ViewLink?type=MET&TorLinkId=310"
+        ]
 
-    H.h1 "Geografiska Kartor"
-    H.p $ commasep
-      [ H.a "Lantmäteriet" H.! A.href "http://kso2.lantmateriet.se"
-      , H.a "Hitta.se"     H.! A.href "http://hitta.se/kartan"
-      , H.a "Eniro"        H.! A.href "http://kartor.eniro.se"
-      , H.a "Bing"         H.! A.href "https://www.bing.com/maps"
-      , H.a "Google"       H.! A.href "https://google.com/maps"
-      ]
+      H.p $ commasep
+        [ "AIP SUP" `ahref` "https://aro.lfv.se/Editorial/View/IAIP?folderId=22"
+        , "NOTAM"   `ahref` "https://aro.lfv.se/Links/Link/ViewLink?TorLinkId=162&type=AIS"
+        ]
+
+      H.h1 "Väderlänkar"
+      H.p $ commasep
+        [ "earth.nullschool.net" `ahref` "http://earth.nullschool.net"
+        , "windyty.com"          `ahref`"https://www.windyty.com"
+        , "wetterzentrale.de"    `ahref` "http://www.wetterzentrale.de"
+        ]
+
+      H.h1 "Geografiska Kartor"
+      H.p $ commasep
+        [ "Lantmäteriet" `ahref` "http://kso2.lantmateriet.se"
+        , "Hitta.se"     `ahref` "http://hitta.se/kartan"
+        , "Eniro"        `ahref` "http://kartor.eniro.se"
+        , "Bing"         `ahref` "https://www.bing.com/maps"
+        , "Google"       `ahref` "https://google.com/maps"
+        ]
   where
-    ahref :: (String, String, String) -> H.Html
-    ahref (u, inner, desc) = H.a (H.toHtml inner) H.! A.href (H.toValue u) H.! A.title (H.toValue desc)
-
     commasep = mconcat . intersperse ", "
+
+    ahref :: String -> String -> H.Html
+    ahref a b = H.a (H.toHtml a) H.! A.href (H.toValue (urlenc b))
+
+    fromLink :: Link -> H.Html
+    fromLink link = linkTitle link `ahref` linkUrl link H.! title
+      where
+        title = A.title (H.toValue (linkTitle link))
 
     tableRow heading links = H.tr $ do
       H.td (H.toHtml heading)
-      H.td (commasep $ map (ahref . fromLink) links)
+      H.td (commasep $ map fromLink links)
 
-    fromLink :: Link -> (String, String, String)
-    fromLink link = (encodeWith escape (url link), title link, extra link)
+    style = do
+      Clay.body Clay.? do
+        Clay.maxWidth (Clay.px 800)
+        Clay.margin Clay.nil Clay.auto Clay.nil Clay.auto
 
-    escape c = isAllowed c || c == '/' || c == ':' || c == '?' || c == '&' || c == '#' || c == '='
+      Clay.h1 Clay.? Clay.borderBottom Clay.solid (Clay.px 1) Clay.black
 
-groupLinks :: [Link] -> M.Map String [Link]
-groupLinks = order . M.fromListWith (++) . assoc
+urlenc :: String -> String
+urlenc = encodeWith escape
   where
-    assoc = map (\link -> (group link, [link]))
-    order = M.map (sortOn title)
+    escape c = isAllowed c ||
+      c == '/' ||
+      c == ':' ||
+      c == '?' ||
+      c == '&' ||
+      c == '#' ||
+      c == '='
+
+main :: IO ()
+main = renderHtmlToByteStringIO B.putStrLn compactSite
